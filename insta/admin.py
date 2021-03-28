3cc0f62944f2b389bd7c6381745a618817f07f15
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-28 17:35:01 +0300

    register models

---
+++ insta/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import (
+    Profile,
+    Image
+)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('bio',)
+    
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'image_name', 'profile')

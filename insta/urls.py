7bfd8bacf7989828f2b5a2e789355424533210ab
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-28 17:36:04 +0300

    create view paths

---
+++ insta/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import(
+    home,
+    my_profile,
+    user_profile,
+    edit_profile
+)
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('profile/', my_profile, name='profile'),
+    path('user_profile/', user_profile, name='user_profile'),
+    path('edit_profile/', edit_profile, name='edit_profile')
+]

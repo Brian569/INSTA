6e5e70e54506568be7ad684097ef41323536eeff
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-30 06:36:08 +0300

    use os environment to fetch secret key, api key, config name etc

--- instagram/settings.py
+++ instagram/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=rnejsqbph(grd0&wlck=m)+5iitu&v#s(%*-goyu1y3vc#k^1'
+SECRET_KEY = os.environ.get('MY_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

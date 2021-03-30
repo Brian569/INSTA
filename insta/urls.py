dfb83d7dfd28412b01a87ec08056fbaa0530777a
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-30 06:34:06 +0300

    create user_profile path

--- insta/urls.py
+++ insta/urls.py
@@ -3,12 +3,14 @@ from .views import(
     home,
     my_profile,
     user_profile,
-    edit_profile
+    edit_profile,
+    comments
 )
 
 urlpatterns = [
     path('', home, name='home'),
     path('profile/', my_profile, name='profile'),
     path('user_profile/', user_profile, name='user_profile'),
-    path('edit_profile/', edit_profile, name='edit_profile')
+    path('edit_profile/', edit_profile, name='edit_profile'),
+    path('comments', comments, name='comments')
 ]

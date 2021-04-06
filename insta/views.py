769fe94605fe699217cb0b27f5dbe5c28a1dce6f
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-06 12:29:15 +0300

    define profile and update profile functions

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Profile, Image
+from .models import Profile, Image, Follow, User, Comment
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from .forms import *
 
 
 @login_required
@@ -14,63 +15,53 @@ def home(request):
     return render(request, 'home.html', context)
 
 @login_required
-def my_profile(request):
-    
-    profile = request.GET.get('profile')
-    if profile == None:
-        photo = Image.objects.all()
-
-    else:
-        photo = Image.objects.filter(profile__name__contains=profile)
-
-    profile = Profile.objects.all()
-
-    context = {'profiles': profile, 'photos': photo}
-    prof_temps = {''}
-
-    return render(request, 'my_accounts/profile.html',context)
-
-@login_required
-def comments(request):
-    if request.method == 'POST':
-        commentss = request.POST['comment']
-        comment = Image.objects.all()
-        comment.create(comments=commentss)
-        
-        print('comment saved')    
-
-    else:
-        photo = Image.objects.all()
-    
-
-    context = { "photos" : photo}
-
-    return render(request, 'comments.html', context)
-
-def login(request):
-
-    return render(request, 'my_accounts/login.html')
-
-def register(request):
-
-    
-    return (request, 'my_accounts/register.html')
-
-
-def likes(request, pk):
-    if request.method == 'POST':
-        like = request.POST['likes']
-        liky = Image.objects.filter(pk = pk)
-        liky.save()
-
-        print(';like saved')
-
-    else:
-       liky = Image.objects.all() 
-
-    return render(request, 'my_accounts/profile.html', {'likes': liky})
-
 
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+@login_required
+def profile(request, prof_id):
+	user=User.objects.get(pk=prof_id)
+	images = Image.objects.filter(profile = prof_id)
+	title = User.objects.get(pk = prof_id).username
+	profile = Profile.objects.filter(user = prof_id)
+
+	if Follow.objects.filter(user_from=request.user,user_to = user).exists():
+		is_follow = True
+	else:
+		is_follow = False
+
+	followers = Follow.objects.filter(user_to = user).count()
+	following = Follow.objects.filter(user_from = user).count()
+	
+
+	return render(request,'my_accounts/profile.html',{"images":images,"profile":profile,"title":title,"is_follow":is_follow,"followers":followers,"following":following})
+	
+
+@login_required
+def updateProfile(request):
+
+	current_user = request.user
+
+	if request.method == 'POST':
+		if Profile.objects.filter(user_id = current_user).exists():
+			form = UpdateProfile(request.POST,request.FILES,instance = Profile.objects.get(user_id = current_user))
+		
+		else:
+			form = UpdateProfile(request.POST, request.FILES)
+
+		if form.is_valid():
+			user_profile = form.save(commit=False)
+			user_profile.user = current_user
+			user_profile.save()
+
+			return redirect('profile', current_user.id)
+
+	else:
+		if Profile.objects.filter(user_id = current_user).exists():
+			form = UpdateProfile(instance = Profile.objects.get(user_id = current_user))
+		else:
+			form = UpdateProfile()
+
+	return render(request, 'my_accounts/update_profile.html', {"form": form})
